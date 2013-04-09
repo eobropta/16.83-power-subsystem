a0433d893aba204dbeb959e7073127655b32974b
Troy Astorino <troyastorino@gmail.com>
2013-04-09 01:23:47 -0400

    Finialized holodeck slides

---
+++ technical-presentation/holodeck-calculations/gen_energy_charts.m
@@ -0,0 +1,16 @@
+dl = 179;
+sms = 89;
+dc = 219;
+
+duration_1 = [30 10 13];
+power_1 = [dc dl dc sms];
+
+duration_2 = [17 10 (orbit_period - eclipse - 17 - 10) 30];
+power_2 = [dc dl sms dc sms];
+
+
+orbit_period = 93.59;
+eclipse = 35.92;
+
+energy_chart(power_1, duration_1, orbit_period, eclipse)
+energy_chart(power_2, duration_2, orbit_period, eclipse)
