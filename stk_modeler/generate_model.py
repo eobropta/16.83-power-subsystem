268ae6c6080c3d288ff62b135454728afc9ab5e0
Eddie Obropta <eobropta@gmail.com>
2013-04-23 23:34:22 -0400

    STK modeler produces an mdl file that has rotating solar arrays

--- stk_modeler/generate_model.py
+++ stk_modeler/generate_model.py
@@ -51,49 +51,55 @@ plate_side_str = polygon('PlateSide','gold4',plate_side)
 plate_sa_str = polygon('PlateSa','blue',plate_sa) 
 
 ## Write Bus 
-# f.write('Component ' + 'PlateBase' + '\n')
-# f.write(plate_base_str)
-# f.write('EndComponent\n')
+f.write('Component ' + 'PlateBase' + '\n')
+f.write(plate_base_str)
+f.write('EndComponent\n')
 
-# f.write('Component ' + 'PlateSide' + '\n')
-# f.write(plate_side_str)
-# f.write('EndComponent\n')
+f.write('Component ' + 'PlateSide' + '\n')
+f.write(plate_side_str)
+f.write('EndComponent\n')
 
 ## Write SolarArray
 f.write('Component ' + 'PlateSa' + '\n')
 f.write('SolarPanel sa\n')
+#f.write('Pointing SolarPoint 0 0 1\n Articulation SolarPoint\n yRotate Rotate 0 0 0\n EndArticulation\n')
+#f.write('Pointing SolarPoint 0 0 1\n')
 f.write(plate_sa_str)
 f.write('EndComponent\n')
 
 
-# #rotation matrix for base
-# R = np.array([[0,0,0],[0,0,0]])
-# #translation matrix for base
-# T = np.array([[0,0,0],[0,0,bus_z]])
-# name = 'PlateBase'
-# base_str = transform(name,R,T)
+#rotation matrix for base
+R = np.array([[0,0,0],[0,0,0]])
+#translation matrix for base
+T = np.array([[0,0,0],[0,0,bus_z]])
+name = 'PlateBase'
+base_str = transform(name,R,T)
 
-# f.write('Component ' + 'Base' + '\n')
-# f.write(base_str)
-# f.write('EndComponent\n')
+f.write('Component ' + 'Base' + '\n')
+f.write(base_str)
+f.write('EndComponent\n')
 
-# #rotation matrix for base
-# R = np.array([[0,90,0],[0,90,0],[0,90,90],[0,90,90]])
-# #translation matrix for base
-# T = np.array([[-bus_x/2.0,0,bus_z/2.0],[bus_x/2.0,0,bus_z/2.0],[0,bus_y/2.0,bus_z/2.0],[0,-bus_y/2.0,bus_z/2.0]])
-# name = 'PlateSide'
-# side_str = transform(name,R,T)
+#rotation matrix for base
+R = np.array([[0,90,0],[0,90,0],[0,90,90],[0,90,90]])
+#translation matrix for base
+T = np.array([[-bus_x/2.0,0,bus_z/2.0],[bus_x/2.0,0,bus_z/2.0],[0,bus_y/2.0,bus_z/2.0],[0,-bus_y/2.0,bus_z/2.0]])
+name = 'PlateSide'
+side_str = transform(name,R,T)
 
-# f.write('Component ' + 'Side' + '\n')
-# f.write(side_str)
-# f.write('EndComponent\n')
+f.write('Component ' + 'Side' + '\n')
+f.write(side_str)
+f.write('EndComponent\n')
 
 ## Write SA
 
 #rotation matrix for sa
-R = np.array([[20,0,90],[20,0,90],[200,0,90],[200,0,90]])
+sa_angle = 0
+R = np.array([[sa_angle,0,90],[sa_angle,0,90],[sa_angle+180,0,90],[sa_angle+180,0,90]])
 #translation matrix for sa
 T = np.array([[0.0,bus_y/2.0+sa_y/2.0,bus_z/2.0],[0.0,-(bus_y/2.0+sa_y/2.0),bus_z/2.0],[0.0,bus_y/2.0+sa_y/2.0,bus_z/2.0+.01],[0.0,-(bus_y/2.0+sa_y/2.0),bus_z/2.0+.01]])
+
+#T = np.array([[0.0,bus_y/2.0+sa_y/2.0,0.0],[0.0,-(bus_y/2.0+sa_y/2.0),0.0],[0.0,bus_y/2.0+sa_y/2.0,0.01],[0.0,-(bus_y/2.0+sa_y/2.0),0.01]])
+
 name = 'PlateSa'
 solar_array_str = transform(name,R,T)
 
@@ -101,13 +107,19 @@ f.write('Component ' + 'SolarArray' + '\n')
 f.write(solar_array_str)
 f.write('EndComponent\n')
 
-#add to body
-#body = 'Component Body\n Refer\n Component Side\n EndRefer\n Refer\n Component Base\n EndRefer\n Refer\n Component SolarArray\n EndRefer\n  EndComponent\n'
+# f.write('Component ' + 'SolarArraySet' + '\n')
+# f.write('Translate 0.0 0.0 ' + str(bus_z/2.0) +'\n')
+# f.write('Refer\n')
+# f.write('Articulation SolarPoint\n yRotate Rotate 0 -360 360\n EndArticulation\n Pointing SolarPoint 0 0 1\n Component SolarArray\n')
+# f.write('EndRefer\n')
+# f.write('EndComponent\n')
 
-body = 'Component Body\n Refer\n Component SolarArray\n EndRefer\n  EndComponent\n'
+#add to body
+body = 'Component Body\n  Refer\n Component Side\n EndRefer\n Refer\n Component Base\n EndRefer\n Refer\n Component SolarArray\n EndRefer\n  EndComponent\n'
+
 
 f.write(body)
 #write root
-root = 'Component holodeck\n Root\n Refer\n Component Body\n EndRefer\n EndComponent\n'
+root = 'Component holodeck\n Root\n Refer\n Articulation SolarPoint\n yRotate Rotate -360 0 360\n EndArticulation\n Pointing SolarPoint 0 0 1\n Component Body\n EndRefer\n EndComponent\n'
 f.write(root)
     
