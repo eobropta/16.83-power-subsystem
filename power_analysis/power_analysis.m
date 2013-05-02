011a20d763f14d58175e45913f2e45f0a3291ba2
Eddie Obropta <eobropta@gmail.com>
2013-05-01 20:20:04 -0400

    Added nov_135 and nov_315

--- power_analysis/power_analysis.m
+++ power_analysis/power_analysis.m
@@ -15,12 +15,26 @@ dl_time = 10; %min
 rotation_time = 1; %min
 max_dc_time = 10; %min
 
-mode_power = [dc sms dl sms dc];
-mode_duration = [max_dc_time rotation_time dl_time rotation_time ...
-    (orbit_duration - max_dc_time - 2*rotation_time - dl_time)]*60;
+% mode_power = [dc sms dl sms dc];
+% mode_duration = [max_dc_time rotation_time dl_time rotation_time ...
+%     (orbit_duration - max_dc_time - 2*rotation_time - dl_time)]*60;
+
+% dc_time1 = 35.31;
+% dc_time2 = 0;
+% sms_time2 = orbit_duration - (2*dl_time + 3*rotation_time + dc_time1 + dc_time2);
+% 
+% mode_power = [dl sms dc sms dl sms dc sms];
+% mode_duration = [dl_time rotation_time dc_time1 rotation_time ...
+%     dl_time rotation_time dc_time2 sms_time2]*60;
+
+dc_time = 44;
+sms_time = orbit_duration - (dl_time + rotation_time + dc_time);
+
+mode_power = [dl sms dc sms];
+mode_duration = [dl_time rotation_time dc_time sms_time]*60;
 
 
-stk_mat_file = '../stk_power_data/case_y45_z45_sept_315';
+stk_mat_file = '../stk_power_data/case_y45_z45_low_june';
 
 
 x_solar = 0.8;
